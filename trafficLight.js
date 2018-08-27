830b40ed1749b79a997e8b54e46df54c455980e7
GrandEnfant <37689523+GrandEnfant@users.noreply.github.com>
2018-08-27 18:43:33 +0300

    v2
    Добавила 3 кружочка. Почему то все зеленые(

--- trafficLight.js
+++ trafficLight.js
@@ -5,4 +5,17 @@ ctx = trafficLight.getContext('2d');
 ctx.fillStyle = "grey" ;
  ctx.fillRect (250, 0, 250, 750);
 
-ctx.arc(125, 187.5, 125, 1.1*Math.PI, 1.9*Math.PI);
+
+ctx.arc(375, 187.5, 100, 0*Math.PI, 2*Math.PI, true);
+ctx.strokeStyle = "red";
+ctx.stroke();
+
+
+ctx.arc(375, 425, 100, 0*Math.PI, 2*Math.PI, true);
+ctx.strokeStyle = "yellow";
+ctx.stroke();
+
+
+ctx.arc(375, 650, 100, 0*Math.PI, 2*Math.PI, true);
+ctx.strokeStyle = "green";
+ctx.stroke();
