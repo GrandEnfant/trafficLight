30efe570c19cff254b3de8c9c37f1e9cf63d32d7
GrandEnfant <37689523+GrandEnfant@users.noreply.github.com>
2018-08-31 19:06:50 +0300

    error
    document is not  difined

--- trafficLight.js
+++ trafficLight.js
@@ -1,3 +1,4 @@
+/*
 var trafficLight = document.getElementById ("trafficLight");
 ctx = trafficLight.getContext('2d'); 
 
@@ -19,3 +20,15 @@ ctx.stroke();
 ctx.arc(375, 650, 100, 0*Math.PI, 2*Math.PI, true);
 ctx.strokeStyle = "green";
 ctx.stroke();
+*/
+
+var test = document.getElementsByClassName ("redLight");
+var time = 0;
+function func() {
+ test.style.marginLeft = time + 'px'; 
+ time = time + 3;
+ } 
+setInterval(func, 5000);
+ 
+
+//document.getElementByClass ("redLight").style.backgroundColor = "grey";
